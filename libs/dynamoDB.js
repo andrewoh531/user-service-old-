2a0875fe8e2a46d1d5856398c449904a023bcc99
Andrew Oh <andrewoh@dius.com.au>
2016-07-21 15:25:06 +1000

    Adding tests and cleaning up promisified vogel

--- libs/dynamoDB.js
+++ libs/dynamoDB.js
@@ -1,6 +1,21 @@
 import AWS from 'aws-sdk';
-import vogels from './vogelsPromisified';
+import Promise from 'bluebird';
+import vogels from 'dynogels';
 import config from './config';
+
+Promise.promisifyAll(require('dynogels/lib/table').prototype);
+Promise.promisifyAll(require('dynogels/lib/item').prototype);
+Promise.promisifyAll(require('dynogels/lib/query').prototype);
+Promise.promisifyAll(require('dynogels/lib/scan').prototype);
+Promise.promisifyAll(require('dynogels/lib/parallelScan').prototype);
+
+const vogels_model = vogels.model;
+vogels.model = function(name, model){
+  if (model) { Promise.promisifyAll(model); }
+  return vogels_model.apply(vogels, arguments);
+};
+
+Promise.promisifyAll(vogels);
 
 const dynamo = initializeDynamoDB();
 vogels.dynamoDriver(dynamo);
--- tests/getOrCreateUserTest.js
+++ tests/getOrCreateUserTest.js
@@ -1,5 +1,7 @@
 import tester from 'lambda-tester';
 import {expect} from 'chai';
+
+import UserModel from '../libs/UserModel';
 import func from '../functions/getOrCreateUser/src/index';
 
 describe('getOrCreateUser', () => {
@@ -18,21 +20,42 @@ describe('getOrCreateUser', () => {
       });
   });
 
-  it('should create user if user does not exists', () => {
-    return tester(func)
-      .event(payload)
-      .expectResult(res => {
-        expect(res).to.deep.equal(payload);
-      });
+  it('should create user if user does not exists', done => {
+
+    UserModel.getAsync(payload.userId)
+      .then(res => {
+        expect(res).to.equal.null;
+        return tester(func)
+          .event(payload)
+          .expectResult(res => {
+            expect(res).to.deep.equal(payload);
+          });
+      })
+      .then(() => UserModel.getAsync(payload.userId))
+      .then(res => {
+        expect(JSON.parse(JSON.stringify(res))).to.deep.equal(payload);
+        done();
+      })
+      .catch(done);
   });
 
-  // it('should create user when user does not exist', () => {
-  //   const newUser = Object.assign({}, payload, {userId: 'newUserId'});
-  //   return tester(func)
-  //     .event(payload)
-  //     .expectResult(res => {
-  //       expect(res).to.deep.equal({key: 'a', value: 'oh'});
-  //       // console.log(`res = ${JSON.stringify(res)}`);
-  //     });
-  // });
+   it('should retrieve existing user when user already exist', done => {
+     const newUser = {
+       userId: '123',
+       name: 'SecondUser',
+       email: 'second@user.com'
+     };
+     tester(func)
+       .event(payload)
+       .expectResult()
+       .then(() => {
+         return tester(func)
+           .event(newUser)
+           .expectResult(res => {
+             expect(res).to.deep.equal(payload);
+             done();
+           });
+       })
+       .catch(done);
+   });
 });
